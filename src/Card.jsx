c36dbb8c06e116926a251092a6ab356ce971c70c
KrisLu <46599862+llllrt@users.noreply.github.com>
2025-04-17 15:42:18 +0800

    完成卡片组件

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,7 @@ function Card() {
         <div className="card">
             <img className='card-image' src={profilePic} alt="profile picture" />
             <h2 className='card-title'>Jason Tatum</h2>
-            <p className='card-text'>杰森·克里斯多福·塔图姆，出生于美国密苏里州东部城市圣路易斯，为现役美国职业篮球运动员，目前效力于NBA联盟的波士顿凯尔特人。</p>
+            <p className='card-text'>杰森·克里斯多福·塔图姆，出生于美国密苏里州东部城市圣路易斯，为现役美国职业篮球运动员，目前效力于NBA联盟的波士顿凯尔特人,于2024年获得NBA总冠军。</p>
         </div>
     );
 }
