f5be83c5b7f88db4e084ddaa76cda09a514aaf6f
Kris <46599862+llllrt@users.noreply.github.com>
2025-04-22 15:52:49 +0800

    点击图片

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,19 +2,12 @@
 
 function Button(){
 
-  let count = 0;
-  // const handleClick = () => console.log("OUCH!");
-  const handleClick = (name) => {
-    if (count < 3) {
-      count++;
-      console.log(`${name} you clicked me ${count} time/s`);
-    } else {
-      console.log(`${name} stop clicking me!`);
-    }
+  const handleClick = (e) => {
+    e.target.textContent = "OUCH!😢";
   }
 
   return (
-    <button onClick={() => handleClick("Kris")}>
+    <button onDoubleClick={(e) => handleClick(e)}>
       Click me 😊
     </button>
   )
