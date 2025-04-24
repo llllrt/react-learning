43e0af761d8fc1e5854b45a38065b3890eca2e4d
Kris <46599862+llllrt@users.noreply.github.com>
2025-04-24 12:06:07 +0800

    onChange与表单练习

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,27 +2,66 @@ import { useState } from "react";
 
 function MyComponent(){
 
+
   const [name, setName] = useState("Guest");
-  const [age, setAge] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("");
 
-
-  const updateName = () => {
-    setName("Kris");
+  function handleNameChange(e){
+    setName(e.target.value);
   }
 
-  const incrementAge = () => {
-    setAge(age + 1);
+  function handleQuantityChange(e){
+    setQuantity(e.target.value);
+  }
+
+  function handleCommentChange(e){
+    setComment(e.target.value);
+  }
+
+  function handlePaymentChange(e){
+    setPayment(e.target.value);
+  }
+
+  function handleShippingChange(e){
+    setShipping(e.target.value);
   }
 
   return(
     <div>
+      <input value={name} onChange={handleNameChange} />
       <p>Name: {name}</p>
-      <button onClick={updateName}>Change Name</button>
+      <input value={quantity} onChange={handleQuantityChange} type="number" />
+      <p>Quantity: {quantity}</p>
+      <textarea value={comment} onChange={handleCommentChange} placeholder="Enter delivery instructions" />
+      <p>Comment: {comment}</p>
+      <select value={payment} onChange={handlePaymentChange}>
+        <option value="">Select an option</option>
+        <option value="Visa">Visa</option>
+        <option value="Mastercard">Mastercard</option>
+        <option value="Giftcard">Giftcard</option>
+      </select>
+      <p>Payment: {payment}</p>
 
-      <p>Age: {age}</p>
-      <button onClick={incrementAge}>Increment Age</button>
+      <label>
+        <input type="radio" value="Pick Up"
+        checked={shipping === "Pick Up"}
+        onChange={handleShippingChange}
+        />
+        Pick Up
+      </label>
+      <label>
+        <input type="radio" value="Delievry"
+        checked={shipping === "Delievry"}
+        onChange={handleShippingChange}
+        />
+        Delievry
+      </label>
+      <p>Shipping: {shipping}</p>
     </div>
   )
 }
 
-export  default MyComponent;
+export default MyComponent;
