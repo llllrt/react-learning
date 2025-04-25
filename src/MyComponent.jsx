c99eeedcc7461c676a0d606feeab5a47c940fb08
KrisLu <46599862+llllrt@users.noreply.github.com>
2025-04-25 19:00:43 +0800

    updater数组增添功能

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,29 +4,28 @@ import { useState } from "react";
 
 function MyComponent() {
     
-    const [car, setCar] = useState({year: 2025, make: "xiaomi", model: "su7Ultra"})
+    const [foods, setFoods] = useState(["pizza", "burger", "pasta"]);
 
-    function handleYearChange(e) {
-        
-        setCar(prevCar => ({...prevCar, year: e.target.value}))
+    function handleAddFood() {
+
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
+        setFoods(foods => [...foods, newFood]);
+        // 不选择setFoods([...foods, newFood])是考虑到React的异步更新机制，如果进行多次调用时foods可能还是 旧的 值，进而导致最终的状态不是你的预期。
+        // 而是选择setFoods(foods => [...foods, newFood])是因为我们需要在函数中使用最新的状态值。
     }
-    function handleMakeChange(e) {
-        setCar(prevCar => ({...prevCar, make: e.target.value}))
+    function handleRemoveFood(index) {
+        setFoods(foods.filter((_, i) => i !== index));
     }
-    function handleModelChange(e) {
-        setCar(prevCar => ({...prevCar, model: e.target.value}))
-    }
-
-
-    return (
-
-        <div>
-            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
-
-            <input type="number" value={car.year} onChange={handleYearChange}/><br />
-            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
-            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
-       </div>
-    )
+    return (<div>
+        <h2>List of Food</h2>
+        <ul>
+            {foods.map((food, index) => (
+                <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>
+            ))}
+        </ul>
+        <input type="text" id="foodInput" placeholder="Enter food name" />
+        <button onClick={handleAddFood}>Add Food</button>
+    </div>)
 }
 export default MyComponent
